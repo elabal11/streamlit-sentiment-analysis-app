b354750abff9508fc7430cb6e168573eae789b5a
El Abbassy <m711418@mac-00404.local>
2023-06-12 11:46:18 +0200

    Initial commit

---
+++ streamlit_app_sentiment_analysis.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+from textblob import TextBlob
+import io
+
+# Function to perform sentiment analysis
+def perform_sentiment_analysis(df):
+    df['polarity'] = df['text'].apply(lambda x: TextBlob(x).sentiment.polarity)
+    df['subjectivity'] = df['text'].apply(lambda x: TextBlob(x).sentiment.subjectivity)
+    return df
+
+st.title('Sentiment Analysis App')
+
+uploaded_file = st.file_uploader('Please upload your excel file', type=['xlsx'])
+
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    df_processed = perform_sentiment_analysis(df)
+
+    # Prepare processed data for download
+    towrite = io.BytesIO()
+    downloaded_file = pd.ExcelWriter(towrite, engine='openpyxl') 
+    df_processed.to_excel(downloaded_file, index=False, sheet_name='Sheet1')
+    downloaded_file.save()
+    downloaded_file = towrite.getvalue()  # Converts the bytes IO stream to actual bytes
+
+    # Use st.download_button to create a download button
+    st.download_button(
+        label="Download Processed Data",
+        data=downloaded_file,
+        file_name="processed_data.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    )
